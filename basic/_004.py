5f2cd6c9a2555c154fccd90f1f2a78ce6316b0c4
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-09 19:55:52 -0300

    program to find cube of a number

---
+++ basic/_004.py
@@ -0,0 +1,6 @@
+#Program to find cube of a number.
+# When an integer is multiplied by itself three times the resultant number is known as its cube number.
+
+number = float(input("Enter a number to find its cube: "))
+cube = number * number * number 
+print("The cube of the number {0} is: {1}".format(number, cube))
