9c6f051ca28650102bd956b7d9e8d06b645b870b
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-14 15:02:00 -0700

    Program to check if a number is even or odd

---
+++ basic/_008.py
@@ -0,0 +1,9 @@
+#Program to find even and odd numbers
+user_input = int(input("Enter a number: "))
+
+if(user_input % 2 == 0):
+    print(f"{user_input} is an EVEN number")
+else:
+    print(f"{user_input} is an ODD number")
+
+
