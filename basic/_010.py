ef2c3e11e3747ad6c12e6235149e01c60154511d
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-16 04:58:44 -0700

    Program to find negative and positive

---
+++ basic/_010.py
@@ -0,0 +1,7 @@
+#Program to find negative and positives
+number = int(input("Enter a number to check if it is positive or negative: "))
+
+if(number < 0): print(f"Negative number: {number}")
+elif(number == 0): print("Zero")
+else: print(f"Positive number: {number}")
+
