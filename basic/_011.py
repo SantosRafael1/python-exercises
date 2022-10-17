edff490858c27b0730bfa63629ef0a87db317af5
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-17 16:51:04 -0300

    Spell check

---
+++ basic/_011.py
@@ -0,0 +1,14 @@
+#Spell check of the word rainbow
+
+first = str(input("First letter: "))
+second = str(input("Second letter: "))
+third = str(input("Third letter: "))
+fourth = str(input("Fourth letter: "))
+fifth = str(input("Fifth letter: "))
+sixth = str(input("Sixth letter: "))
+seventh = str(input("Seventh letter: "))
+
+if (first == 'R'.upper()) and (second == 'A'.upper()) and (third == 'I'.upper()) and (fourth == 'N'.upper()) and (fifth == 'B'.upper()) and (sixth == 'O'.upper()) and (seventh == 'W'.upper()):
+    print("The spelling is right")
+else:
+    print("The spelling is wrong")
