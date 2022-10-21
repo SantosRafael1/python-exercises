8d2746e844da15c4cf1eb356c16951c9ddd373e9
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-21 09:29:14 -0300

    Program to check if a number is weird or not

---
+++ basic/_014.py
@@ -0,0 +1,8 @@
+#Program to check if a number is weird or not
+
+n = int(input().strip())
+    
+if (n % 2 != 0): print("Weird")
+if(n % 2 == 0) and (n >= 2) and (n <= 5): print("Not Weird")
+if(n % 2 == 0) and (n >= 6) and (n <= 20): print("Weird")
+if(n % 2 == 0) and (n > 20): print("Not Weird")
