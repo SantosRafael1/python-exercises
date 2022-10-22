3a03b5c48694aa0c83432b160a7b02f2ef0c545c
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-22 14:57:05 -0300

    Program to read an integer and prints a squared number

---
+++ basic/_015.py
@@ -0,0 +1,10 @@
+#The provided code stub reads and integer, n, from STDIN. For all non-negative integers 
+# i < n, print i².
+
+n = int(input())
+
+i = 0
+while(i < n):
+    print(i ** 2)
+    i += 1
+    
