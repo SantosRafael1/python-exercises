7d7a9f86b3c0a75d6c3008f3b729f278eea5150b
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-28 20:12:55 -0300

    Programa para calcular dois pontos

---
+++ basic/_018.py
@@ -0,0 +1,9 @@
+#Distância entre dois pontos
+
+import math
+
+x1, y1 = [float(x) for x in input().split()]
+x2, y2 = [float(x) for x in input().split()]
+
+distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+print("%.4f" % distance)
